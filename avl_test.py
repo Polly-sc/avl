06db8e6b20448c7885f151c1eb578f834e4e682c
Polly-sc <paralelepiped1999@mail.ru>
2020-12-12 09:25:10 +0300

    Тест инициализация дерева и его узла

---
+++ avl_test.py
@@ -0,0 +1,17 @@
+import unittest
+from AVLtree import AVLTree, Node
+
+class AVLtest(unittest.TestCase):
+#инициализация дерева
+    def test_tree_init(self):
+        avl_tree = AVLTree()
+        self.assertIsNone(avl_tree.root, None)
+
+#инициализация узла дерева
+    def test_node_init(self):
+        node = Node(15)
+        self.assertEqual(node.key, 15)
+        self.assertTrue(node.red)
+        self.assertIsNone(node.right, None)
+        self.assertIsNone(node.left, None)
+        self.assertIsNone(node.parent, None)
